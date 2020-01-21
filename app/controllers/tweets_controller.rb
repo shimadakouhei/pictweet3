63e8a91607894457287a6de9d009f2a9fe5b7599
shimadakouhei <shimapykkm@gmail.com>
2020-01-21 15:32:14 +0900

    新規投稿機能の実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,20 @@
 class TweetsController < ApplicationController
   def index
     @tweets = Tweet.all
-  end  
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    # binding.pry
+    Tweet.create(tweet_params)
+  end
+  
+  private
+  def tweet_params
+    params.require(:tweet).permit(:name, :image, :text)
+  end
+
 end
