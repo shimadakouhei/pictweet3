75b3ba19db68a8cfb60ff8034ae67aa282800093
shimadakouhei <shimapykkm@gmail.com>
2020-02-22 01:18:43 +0900

    Update tweets_controller.rb

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,6 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: [:edit, :show]
   before_action :move_to_index, except: [:index, :show, :search]
-
   def index
     @tweets = Tweet.includes(:user).order("created_at DESC").page(params[:page]).per(5)
   end
@@ -31,20 +30,16 @@ class TweetsController < ApplicationController
     @comment = Comment.new
     @comments = @tweet.comments.includes(:user)
   end
-
   def search
     @tweets = Tweet.search(params[:keyword])
   end
-
   private
   def tweet_params
     params.require(:tweet).permit(:image, :text).merge(user_id: current_user.id)
   end
-
   def set_tweet
     @tweet = Tweet.find(params[:id]) 
   end  
-
   def move_to_index
     redirect_to action: :index unless user_signed_in?
   end
