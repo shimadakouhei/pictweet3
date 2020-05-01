b777b12d979be2ad9b6c944d7e2141ebfc4a151d
shimadakouhei <shimapykkm@gmail.com>
2020-05-01 10:39:08 +0900

    コントローラー作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,7 +4,6 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.includes(:user).order("created_at DESC").page(params[:page]).per(5)
   end
-
   def new
     @tweet = Tweet.new
   end
